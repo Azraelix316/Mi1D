6eb52c9b0657f0063ee9c1d8192db5ade1c831e4
Azraelix316 <jaredj13310@gmail.com>
2024-11-24 19:16:48 +0800

    Adds stochastic model

--- Disease-Outbreaks/model.cpp
+++ Disease-Outbreaks/model.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <ostream>
 #include <vector>
+#include <random>
 using namespace std;
 //Model 1: 11/24 Predicting Disease Outbreaks
 //Main source: https://www.youtube.com/watch?v=m6Hr69JH_wA
@@ -10,7 +11,7 @@ double susceptible=1000;
 double infected=1;
 double removed=0;
 double b=0.0001;
-double u=0.01;
+double u=0.02;
 int counter=0;
 int main() {
 //Solution 1: ODE
@@ -18,13 +19,23 @@ fstream fout;
 fout.open("data.csv",ios::out);
 double R0=b*susceptible*1/u;
         fout << "susceptible" << "," << "infected" <<","<< "removed" << ","<< "time" <<"\n";
-    while (susceptible>0 && counter<1000) {
+    // while (infected>0 && counter<1000) {
+    //     fout << susceptible << "," << infected <<","<< removed << "," << counter <<"\n";
+    //     susceptible+=ceil(infected*-b*susceptible);
+    //     infected+=ceil(susceptible*b*infected-u*infected);
+    //     removed+=ceil(u*infected);
+    //     counter++;
+    // }
+//Solution 2: ODE + Stochastic
+    while (infected>0 && counter<1000) {
         fout << susceptible << "," << infected <<","<< removed << "," << counter <<"\n";
-        susceptible+=infected*-b*susceptible;
-        infected+=susceptible*b*infected-u*infected;
-        removed+=u*infected;
+        //able to go from 0-2x infection, average is the same
+        double randomInfected=rand()/32767.00*b*susceptible*infected*2;
+        double randomRecovered=rand()/32767.00*u*infected*2;
+        susceptible-=ceil(randomInfected);
+        infected+=ceil(randomInfected-randomRecovered);
+        removed+=ceil(randomRecovered);
         counter++;
     }
-
 
 }
